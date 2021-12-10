fb91dc836287a0a32a90940a12f6126440962fac
Angelo Michel <angelo.michel@frontmen.nl>
2021-12-10 12:18:04 +0100

    import example

---
+++ src/importExample.js
@@ -0,0 +1,7 @@
+import { exampleFunction, exampleVariable } from "./anotherFile";
+
+// will return "Example variable value here"
+console.log(exampleVariable);
+
+// will return "Example function with an argument (test)."
+console.log(exampleFunction("test"));
